8eebdb4631fd863210413ac467929567856fbc65
DoubleCheck <68802586+CM8DoubleCheck@users.noreply.github.com>
2021-03-14 01:34:06 -0600

    Added middleware, views & static directory variables

--- routes/Router.js
+++ routes/Router.js
@@ -7,20 +7,21 @@ Base router class for building on top of Express' Router
 const express = require('express')
 const path = require('path')
 const sanitizeHtml = require('sanitize-html')
-const PUBLIC_DIR = path.join(__dirname, '..', 'public') // Define our public directory where we store all HTML files
-
-// Include our other sub-routers here so we can pass them all back to the index file through one variable
-const Base = require('./Base')
-const API = require('./API')
-const Upload = require('./API')
+const middleware = require('../middleware')
+const VIEWS_DIR = path.join(__dirname, '..', 'public') // Define our views directory
+const STATIC_DIR = path.join(__dirname, '..', 'public') // Define our static directory
 
 class Router extends express.Router {
   constructor () {
     super()
     this.express = express
-    this.PUBLIC_DIR = PUBLIC_DIR
+    this.middleware = middleware
+    this.VIEWS_DIR = VIEWS_DIR
+    this.STATIC_DIR = STATIC_DIR
     this.sanitizeHTML = sanitizeHtml()
+
+    // Apply our global middleware to ALL pages
+    this.use(middleware.global)
   }
 }
-
-module.exports = { Router, Base, API, Upload }
+module.exports = Router
