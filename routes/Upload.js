0661d9e75795f0f45442b073510cfeec9f7f9b0a
DoubleCheck <68802586+CM8DoubleCheck@users.noreply.github.com>
2021-03-15 01:26:42 -0500

    removed con.connect()

--- routes/Upload.js
+++ routes/Upload.js
@@ -76,90 +76,84 @@ class Upload extends Router {
           }
 
           const insertVideoInfo = async () => {
-            con.connect((err) => {
-              if (err) {
-                return console.error(err)
-              }
-              console.log('Video Upload database: Connected!')
 
-              this.Database.insertVideo(name, description, privacy, category, tags, videoId, req.session.user.userLoggedIn, 'uploads/videos/" + id + ".mp4', (err, result) => {
-                if (err) throw err
-                console.log('Video Info inserted')
+            this.Database.insertVideo(name, description, privacy, category, tags, videoId, req.session.user.userLoggedIn, 'uploads/videos/" + id + ".mp4', (err, result) => {
+              if (err) throw err
+              console.log('Video Info inserted')
 
-                console.dir(result)
+              console.dir(result)
 
-                const processThumbnails = async () => {
-                  const thumbnailId = rand(20, 'aA0')
-                  const latestVideoId = result.insertId
+              const processThumbnails = async () => {
+                const thumbnailId = rand(20, 'aA0')
+                const latestVideoId = result.insertId
 
-                  await ffmpeg(outFilename)
-                    .screenshot({
-                      count: 3,
-                      folder: 'uploads/videos/thumbnails',
-                      size: '1280x720',
-                      filename: thumbnailId + '-' + id + '.png'
-                    })
-
-                  const thumbnailVideoId = rand(12, 'aA0')
-                  this.Database.insertThumbnail(id, latestVideoId, thumbnailId, thumbnailVideoId, (err, result) => {
-                    if (err) throw err
-                    console.log('Thumbnail Video inserted')
-                    console.dir(params)
+                await ffmpeg(outFilename)
+                  .screenshot({
+                    count: 3,
+                    folder: 'uploads/videos/thumbnails',
+                    size: '1280x720',
+                    filename: thumbnailId + '-' + id + '.png'
                   })
 
-                  const calculateVideoDuration = async () => {
-                    await ffmpeg.ffprobe(outFilename, (err, metadata) => {
+                const thumbnailVideoId = rand(12, 'aA0')
+                this.Database.insertThumbnail(id, latestVideoId, thumbnailId, thumbnailVideoId, (err, result) => {
+                  if (err) throw err
+                  console.log('Thumbnail Video inserted')
+                  console.dir(params)
+                })
+
+                const calculateVideoDuration = async () => {
+                  await ffmpeg.ffprobe(outFilename, (err, metadata) => {
+                    if (err) throw err
+                    const latestVideoId = result.insertId
+                    // const videoDuration = metadata.streams[0].duration <--- commented out unused variable
+
+                    const duration = metadata.streams[0].duration
+                    let hours = Math.floor(duration / 3600)
+                    let mins = Math.floor((duration - hours * 3600) / 60)
+                    let secs = Math.floor(duration % 60)
+                    hours = hours < 1 ? '' : hours + ':'
+                    mins = mins < 10 ? '0' + mins + ':' : mins + ':'
+                    secs = secs < 10 ? '0' + secs : secs
+                    const finalDuration = hours + mins + secs
+
+                    console.log(finalDuration)
+
+                    console.log(metadata)
+
+                    this.Database.setVideoDuration(latestVideoId, duration, (err, result) => {
                       if (err) throw err
-                      const latestVideoId = result.insertId
-                      // const videoDuration = metadata.streams[0].duration <--- commented out unused variable
-
-                      const duration = metadata.streams[0].duration
-                      let hours = Math.floor(duration / 3600)
-                      let mins = Math.floor((duration - hours * 3600) / 60)
-                      let secs = Math.floor(duration % 60)
-                      hours = hours < 1 ? '' : hours + ':'
-                      mins = mins < 10 ? '0' + mins + ':' : mins + ':'
-                      secs = secs < 10 ? '0' + secs : secs
-                      const finalDuration = hours + mins + secs
-
-                      console.log(finalDuration)
-
-                      console.log(metadata)
-
-                      this.Database.setVideoDuration(latestVideoId, duration, (err, result) => {
-                        if (err) throw err
-                        console.log(result)
-                      })
+                      console.log(result)
                     })
-                  }
-
-                  const deleteInputVideo = async () => {
-                    await fs.unlink(inFilename, (err) => {
-                      if (err) throw err
-
-                      console.log('Video: ' + inFilename + ' is now deleted from the tmp folder')
-
-                      // send response
-                      res.send({
-                        status: true,
-                        message: 'Video is uploaded',
-                        data: {
-                          name: video.name,
-                          mimetype: video.mimetype,
-                          size: video.size,
-                          outputVideo: outFilename,
-                          inputVideo: inFilename,
-                          viewVideo: 'https://winsvideo.net/watch?v=' + videoId + ''
-                        }
-                      })
-                    })
-                  }
-
-                  setTimeout(processThumbnails, 2000)
-                  setTimeout(calculateVideoDuration, 3000)
-                  setTimeout(deleteInputVideo, 4000)
+                  })
                 }
-              })
+
+                const deleteInputVideo = async () => {
+                  await fs.unlink(inFilename, (err) => {
+                    if (err) throw err
+
+                    console.log('Video: ' + inFilename + ' is now deleted from the tmp folder')
+
+                    // send response
+                    res.send({
+                      status: true,
+                      message: 'Video is uploaded',
+                      data: {
+                        name: video.name,
+                        mimetype: video.mimetype,
+                        size: video.size,
+                        outputVideo: outFilename,
+                        inputVideo: inFilename,
+                        viewVideo: 'https://winsvideo.net/watch?v=' + videoId + ''
+                      }
+                    })
+                  })
+                }
+
+                setTimeout(processThumbnails, 2000)
+                setTimeout(calculateVideoDuration, 3000)
+                setTimeout(deleteInputVideo, 4000)
+              }
             })
           }
           setTimeout(convertToMp4, 500)
